e6e6f0048af04f279a70bdb757cb3cc40801b141
Isfandiyor-Vohidov <1984dilshod.abdullayev@gmail.com>
2026-05-08 16:21:03 +0500

    chore: initial monorepo scaffold with Turborepo, ESLint, Prettier, and env config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  ignorePatterns: ['node_modules', 'dist', '.next', '.turbo'],
+};
